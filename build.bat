1c4fda41c6aef2d2fdb10655dac96e53fe17eaa8
Krzem <krzem5.dev@gmail.com>
2021-04-17 13:33:22 +0200

    Push Update 04/17/2021, 13:33:15

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 @echo off
-echo NUL>_.class&&del /s /f /q *.class
 cls
-javac -encoding utf8 -cp com/krzem/color_terminal/modules/jna-5.5.0.jar;com/krzem/color_terminal/modules/jna-platform-5.5.0.jar; com/krzem/color_terminal/Main.java&&java -Dfile.encoding=UTF8 -cp com/krzem/color_terminal/modules/jna-5.5.0.jar;com/krzem/color_terminal/modules/jna-platform-5.5.0.jar; com/krzem/color_terminal/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+python build.py %*
